1a27b5a75a8cfd7f82e1381f0f89a011a5d37b00
Jeff Jarvis <jeffrey.jarvis32@gmail.com>
2022-02-04 19:21:17 -0500

    added nav bar at the top with some styling. added card for results display

---
+++ Assets/script.js
@@ -0,0 +1,33 @@
+// Weather App
+
+//Search Bar
+//function: Event handler
+//1. read the value from the textbook into a veriable
+//2. create URL to fetch
+//3. fetch the results
+//4. Call Function to update after results
+
+
+//Function: DoResults
+//1. Call function add search term to results
+//2. call function add today's results
+//3.call function add 5 day results
+//4. clear out the text box
+
+
+//RECENT SEARCH
+// function: add search term to results
+// parameter in: search term
+//1. add a button to the screen, with a data attribute that stores the search term
+
+// Function: EVENT HANDLER
+//1. read the data attribute to get the search term
+//2. Call search function
+
+
+//RESULTS TODAY
+//function add todays results
+
+
+//Results 5 days
+//function add 5 days results
